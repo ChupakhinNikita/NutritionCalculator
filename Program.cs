2050ca80c602da82087f4f0615ed3f8cabb5e69e
Nikita <n.chupakhin@yandex.ru>
2025-09-21 16:13:53 +0300

    git init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NutritionCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
