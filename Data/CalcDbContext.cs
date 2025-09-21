def472d20ec1753fed34d305a288405078a33600
ChupakhinNikita <n.chupakhin@yandex.ru>
2025-09-21 22:54:52 +0300

    add products list

---
+++ Data/CalcDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NutritionCalculator.Models;
+
+namespace NutritionCalculator.Data
+{
+    internal class CalcDbContext : DbContext
+    {
+        public CalcDbContext(DbContextOptions<CalcDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<RecipeIngredient> RecipeIngredients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasOne(ri => ri.Recipe)
+                .WithMany(r => r.Ingredients)
+                .HasForeignKey(ri => ri.RecipeId);
+
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasOne(ri => ri.Product)
+                .WithMany()
+                .HasForeignKey(ri => ri.ProductId);
+        }
+    }
+}
--- Models/Product.cs
+++ Models/Product.cs
@@ -12,12 +12,12 @@ namespace NutritionCalculator.Models
 
         public string Name { get; set; }
 
-        public double Calories { get; set; }
+        public double Calories { get; set; } // К
 
-        public double Protein { get; set; }
+        public double Protein { get; set; } // Б
 
-        public double Fat { get; set; }
+        public double Fat { get; set; } // Ж
 
-        public double Carbohydrates { get; set; }
+        public double Carbohydrates { get; set; } // У
     }
 }
