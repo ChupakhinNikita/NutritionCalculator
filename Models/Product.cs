b382be11ee9643dea0a272277c81e92c24f16e2d
ChupakhinNikita <n.chupakhin@yandex.ru>
2025-09-21 16:56:26 +0300

    add models

---
+++ Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutritionCalculator.Models
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Calories { get; set; }
+
+        public double Protein { get; set; }
+
+        public double Fat { get; set; }
+
+        public double Carbohydrates { get; set; }
+    }
+}
---
+++ Models/RecipeIngredient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutritionCalculator.Models
+{
+    internal class RecipeIngredient
+    {
+        public int Id { get; set; }
+
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public double Quantity { get; set; }
+    }
+}
